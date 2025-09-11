ba574141892a792eb37be25596319e49e00e2a29
Saharsh Kontham <Saharsh.kontham1@gmail.com>
2025-09-11 13:51:16 +1000

    Adding stuff using copilot

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ app = Flask(__name__)
 def index():
   return render_template('/index.html')
 
+@app.route('/habits', methods=['GET', 'POST'])
+def habits():
+  return render_template('habits.html')
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
 
-  
+
 
 
 
